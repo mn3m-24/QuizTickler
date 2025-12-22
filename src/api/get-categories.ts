97866db27e358382081fa1491159eedc6fbc3b1f
Abdalmonem Salah <abdalmonemshagar@proton.me>
2025-12-22 04:53:19 +0200

    refactor: use the api instead of the json files & restructure some files

--- src/api/get-categories.ts
+++
@@ -1,6 +0,0 @@
-import data from '@/data/categories.json' with { type: 'json' };
-const categories = data['trivia_categories'];
-
-const getCategories = () => categories.map((obj) => obj.name);
-
-export default getCategories;
