348cc56947a6b80e69d1b83b5aa77e615e406c63
abdalmonem <abdalmonemshagar@proton.me>
2026-01-31 21:07:38 +0200

    refactor: use Quiz status for pages switching

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,22 @@
-import useQuiz from '@/hooks/use-quiz-context';
-import QuizPage from '@/pages/quiz-page';
-import ResultPage from '@/pages/result-page';
-import StartPage from '@/pages/start-page';
-import QuizProvider from '@/store/quiz-provider';
-
-function QuizContent() {
-  const {
-    state: { questions, endTime },
-  } = useQuiz();
-
-  if (questions.length === 0) return <StartPage />; // No questions -> starting page
-  if (endTime !== null) return <QuizPage />; // questions & endTime -> quiz page
-  return <ResultPage />; // questions & no endTime -> result page
-}
+import useQuizStore from "./store/use-quiz-store";
+import QuizPage from "@/pages/quiz-page";
+import ResultPage from "@/pages/result-page";
+import StartPage from "@/pages/start-page";
 
 function App() {
-  return (
-    <>
-      <QuizProvider>
-        <QuizContent />
-      </QuizProvider>
-    </>
-  );
+  const status = useQuizStore((state) => state.status);
+  switch (status) {
+    case "idle":
+      return <StartPage />;
+    // case "loading":
+    //   return <LoadingSpinner />;
+    case "active":
+      return <QuizPage />;
+    case "completed":
+      return <ResultPage />;
+    default:
+      return <StartPage />;
+  }
 }
 
 export default App;
