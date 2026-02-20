8d70bc8e05a777fae1925f3c73b93216f8207834
abdalmonem <abdalmonemshagar@proton.me>
2026-02-20 13:21:28 +0200

    feat: add skeleton categories & handle error with toast message

--- src/components/categories.tsx
+++ src/components/categories.tsx
@@ -1,34 +1,40 @@
 import { useCategories } from "@/api/use-categories";
+import Button from "./ui/button";
+import cn from "@/utils/cn";
+import Skeleton from "./ui/skeleton";
+import { toast } from "sonner";
 
 interface CategoriesProp {
   onSelect: (id: string) => void;
   selectedCategory: string | null;
 }
+
 export const Categories = ({ onSelect, selectedCategory }: CategoriesProp) => {
   const { data: categories, error, isLoading } = useCategories();
-  if (error) return <h1>error from categories.tsx</h1>;
-  if (isLoading) return <h1>Loading...</h1>;
+  // TODO use a better error component
+  if (error) toast.error("Error fetching categories");
   return (
-    <div className="category">
-      <label>Category</label>
-      {categories?.map(({ id, name }) => (
-        <button
-          type="button"
-          key={id}
-          onClick={() => onSelect(id.toString())}
-          value={id}
-          style={
-            selectedCategory
-              ? selectedCategory === id.toString()
-                ? { backgroundColor: "blue" }
-                : {}
-              : {}
-          }
-        >
-          {name}
-        </button>
-      ))}
-      <output>{selectedCategory}</output>
-    </div>
+    <section className="flex flex-col items-center gap-4">
+      <h2>Category</h2>
+      <div className="flex flex-wrap justify-center gap-4">
+        {isLoading
+          ? (Array.from({ length: 12 }).map((_, i) => <Skeleton key={i} className="h-9 w-32" aria-hidden="true" />))
+          : categories?.map(({ id, name }) => (
+          <Button
+            variant="secondary"
+            size="sm"
+            className={cn({
+              "bg-blue-500": selectedCategory === id.toString(),
+            })}
+            type="button"
+            key={id}
+            onClick={() => onSelect(id.toString())}
+            value={id}
+          >
+            {name}
+          </Button>
+        ))}
+      </div>
+    </section>
   );
 };
