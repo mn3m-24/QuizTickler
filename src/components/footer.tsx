f516f5d0157b8c8937726a67c2037831002724e6
abdalmonem <abdalmonemshagar@proton.me>
2026-02-20 12:18:43 +0200

    feature: add footer & header components

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+const Footer = () => (
+  <footer className="flex justify-center items-center shadow bg-gray-200">
+    Copyright &copy; {new Date().getFullYear()} Abdalmonem. All rights reserved.
+  </footer>
+);
+
+export default Footer;
